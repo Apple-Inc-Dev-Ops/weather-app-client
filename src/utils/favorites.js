8486b229da4fa424a34f3eb8d1328f4a15f677ca
Pieter Vardi <pietervardi@gmail.com>
2024-11-04 00:33:22 +0700

    fix: fix superlinter error (3)

--- src/utils/favorites.js
+++ src/utils/favorites.js
@@ -1,15 +1,15 @@
 import axios from '../config/axios'
 
+export async function getFavorites() {
+  return (await axios.get(`/favorites/${localStorage.getItem('username')}`)).data
+}
+
 export async function toggleFavorites(lat, long) {
   return (
-    await axios.put(`/favorites/toggle`, {
+    await axios.put('/favorites/toggle', {
       username: localStorage.getItem('username'),
       lat: Number(lat),
       long: Number(long)
     })
   ).data
-}
-
-export async function getFavorites() {
-  return (await axios.get(`/favorites/${localStorage.getItem('username')}`)).data
 }
